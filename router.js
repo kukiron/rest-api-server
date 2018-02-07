42fc650e5b11f37fb9f6718254a7f9f39a682f43
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-02-08 02:42:27 +0600

    path.resolve

--- router.js
+++ router.js
@@ -11,7 +11,7 @@ const requireAuth = passport.authenticate("jwt", { session: false }),
 module.exports = function(app) {
   // View engine
   app.set("view engine", "ejs")
-  app.set("views", path.join(__dirname, "views"))
+  app.set("views", path.resolve(__dirname, "views"))
   // Sample welcome page
   app.get("/", (req, res) => {
     res.render("index", {
